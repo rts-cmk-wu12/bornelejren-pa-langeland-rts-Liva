f958d130d1686ec453aaff04360c9bf1e94ed26c
rts-Liva <U2451012@rts-365.dk>
2025-05-14 12:28:09 +0200

    main

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,6 +16,16 @@ function HomePage() {
                     <Link to="/sponser" className="hero__sponser-btn">Tilmeld som sponser</Link>
                 </div>
             </section>
+            <main>
+                <div className="column">
+                    <p className='column__text'>Der er mange børn i verden, der ikke har det så godt, som de burde have det. Det gælder desværre også i Danmark. Der er børn med medfødte handicap, børn der er blevet alvorligt syge, børn der lider under omsorgssvigt eller bliver udsat for overgreb, børn der har mistet én af eller begge deres forældre, børn der er tvangsfjernet fra deres hjem og familie, børn der bliver mobbet i skolen, børn der vokser op i meget fattige familier, børn fra skilsmisse- og sammenbragte familier - vi kender allesammen et barn, der tilhører en af disse grupper, og det er disse børn, vi gerne vil gøre noget for.</p>
+                    <img src="inside.jpg" alt="Køkken" className="column__img" />
+                </div>
+                <div className="text-section">
+                    <p className='text-section__text'>Foreningen Børnelejren på Langeland samler penge ind fra erhvervslivet, fonde og private til at sende dårligt stillede børn på et velfortjent lejrophold på Søgård.</p>
+                    <p className="text-section__text text-section__text--bold">Hjælp os med at hjælpe dem!</p>
+                </div>
+            </main>
         </>
     );
 }
