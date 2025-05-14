42629f4e06ae1f653a5eebf32a82c7f07e5165fe
rts-Liva <U2451012@rts-365.dk>
2025-05-14 09:42:20 +0200

    basic

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/header';
+import '../scss/components/index.scss';
+
+function HomePage() {
+    return (
+        <>
+            <Header currentPage='index' />
+        </>
+    );
+}
+
+export default HomePage;
