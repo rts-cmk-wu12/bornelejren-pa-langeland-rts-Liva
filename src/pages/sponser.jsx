c7d6eee497771d8f9ec758e1525271b5a74e8c38
rts-Liva <U2451012@rts-365.dk>
2025-05-15 10:06:09 +0200

    component

--- src/pages/sponser.jsx
+++ src/pages/sponser.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
 import '../scss/components/sponser.scss';
+import '../scss/components/intro.scss';
 
 function SponserPage() {
     return (
